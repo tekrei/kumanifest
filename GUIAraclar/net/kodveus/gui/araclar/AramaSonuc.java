49d6fe38dc47dfe0a5d33e8bfae19db0efa0f54b
tekrei <tekrei@7196b984-1229-0410-b02e-49f04d2d2cda>
2007-03-31 08:23:37 +0000

    -AramaSonuc sinifinda sadece fare eylemlerini dinlemek yerine secim eylemlerini dinleme gerceklestirildi boylece klavyede dinleniyor.

--- GUIAraclar/net/kodveus/gui/araclar/AramaSonuc.java
+++ GUIAraclar/net/kodveus/gui/araclar/AramaSonuc.java
@@ -4,126 +4,132 @@
 package net.kodveus.gui.araclar;
 
 import java.awt.BorderLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import net.kodveus.gui.jtable.GenericTableModel;
 import net.kodveus.gui.jtable.JFocusTable;
 import net.kodveus.gui.jtable.JTableMetodlar;
 import net.kodveus.gui.jtable.TableSorter;
 
-
 public class AramaSonuc extends JPanel {
 
-    private static final long serialVersionUID = 1L;
-    public static int SIRALAMALI = 0;
-    public static int NORMAL = 1;
-    JTable jtable;
-    GenericTableModel model;
-    TableSorter sorter;
-    ArrayList liste;
-    AliasMap map;
-    int tabloTipi;
-    private AramaSonucInterface _arayuz;
-    
-    public AramaSonuc(){
-    }
+	private static final long serialVersionUID = 1L;
+	public static int SIRALAMALI = 0;
+	public static int NORMAL = 1;
+	JTable jtable;
+	GenericTableModel model;
+	TableSorter sorter;
+	ArrayList<Object> liste;
+	AliasMap map;
+	int tabloTipi;
+	private AramaSonucInterface _arayuz;
 
-    public AramaSonuc(AliasMap _map) {
-        this(_map, new ArrayList(), NORMAL);
-    }
+	public AramaSonuc() {
+	}
 
-    public AramaSonuc(AliasMap _map, int _tabloTipi) {
-        this(_map, new ArrayList(), _tabloTipi);
-    }
+	public AramaSonuc(AliasMap _map) {
+		this(_map, new ArrayList<Object>(), NORMAL);
+	}
 
-    public AramaSonuc(AliasMap _map, ArrayList _liste, int _tabloTipi) {
-        map = _map;
-        tabloTipi = _tabloTipi;
-        liste = _liste;
-        jtable = new JFocusTable();
-        prepareGUI();
-    }
+	public AramaSonuc(AliasMap _map, int _tabloTipi) {
+		this(_map, new ArrayList<Object>(), _tabloTipi);
+	}
+
+	public AramaSonuc(AliasMap _map, ArrayList<Object> _liste, int _tabloTipi) {
+		map = _map;
+		tabloTipi = _tabloTipi;
+		liste = _liste;
+		jtable = new JFocusTable();
+		prepareGUI();
+	}
 
 	private void prepareGUI() {
 		prepareTable();
 
-        jtable.addMouseListener(new MouseAdapter() {
-                public void mouseReleased(MouseEvent e) {
-                    if (jtable.getSelectedRowCount() > 0) {
-                        if (_arayuz != null) {
-                            _arayuz.setSecili(getSeciliNesne());
-                        }
-                    }
-                }
-            });
+		// FIX Fare dinlemek yerine asagidaki gibi secmeleri dinlemek klavyeyi
+		// de destekliyor
+		jtable.getSelectionModel().addListSelectionListener(
+				new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent e) {
+						// If cell selection is enabled, both row and column
+						// change events are fired
+						if (e.getSource() == jtable.getSelectionModel()
+								&& jtable.getRowSelectionAllowed()
+								&& jtable.getSelectedRowCount() > 0) {
+							if (_arayuz != null) {
+								_arayuz.setSecili(getSeciliNesne());
+							}
+						}
+					}
+				});
 
-        this.setLayout(new BorderLayout());
+		this.setLayout(new BorderLayout());
 
-        JScrollPane jscr = new JScrollPane(jtable);
-        this.add(jscr, BorderLayout.CENTER);
-        this.updateUI();
+		JScrollPane jscr = new JScrollPane(jtable);
+		this.add(jscr, BorderLayout.CENTER);
+		this.updateUI();
 	}
-    
-    public void setAliasMap(AliasMap _map){
-    	map = _map;
-        tabloTipi = NORMAL;
-        liste = new ArrayList();
-        jtable = new JFocusTable();
-        prepareGUI();
-    }
 
-    public void setArayuz(AramaSonucInterface arayuz) {
-        this._arayuz = arayuz;
-    }
+	public void setAliasMap(AliasMap _map) {
+		map = _map;
+		tabloTipi = NORMAL;
+		liste = new ArrayList<Object>();
+		jtable = new JFocusTable();
+		prepareGUI();
+	}
 
-    private void prepareTable() {
-        //jtable = new JFocusTable();
-        model = new GenericTableModel(map, liste, false);
+	public void setArayuz(AramaSonucInterface arayuz) {
+		this._arayuz = arayuz;
+	}
 
-        if (tabloTipi == SIRALAMALI) {
-            sorter = new TableSorter(model);
-            JTableMetodlar.prepareTable(jtable, sorter);
-            sorter.setTableHeader(jtable.getTableHeader());
-        } else {
-            JTableMetodlar.prepareTable(jtable, model);
-        }
+	private void prepareTable() {
+		// jtable = new JFocusTable();
+		model = new GenericTableModel(map, liste, false);
 
-        jtable.updateUI();
-    }
+		if (tabloTipi == SIRALAMALI) {
+			sorter = new TableSorter(model);
+			JTableMetodlar.prepareTable(jtable, sorter);
+			sorter.setTableHeader(jtable.getTableHeader());
+		} else {
+			JTableMetodlar.prepareTable(jtable, model);
+		}
 
-    public Object getSeciliNesne() {
-        if (tabloTipi == SIRALAMALI) {
-            return sorter.getObjectAt(jtable.getSelectedRow());
-        }
+		jtable.updateUI();
+	}
 
-        return liste.get(jtable.getSelectedRow());
-    }
+	public Object getSeciliNesne() {
+		if (tabloTipi == SIRALAMALI) {
+			return sorter.getObjectAt(jtable.getSelectedRow());
+		}
 
-    public ArrayList getSecili() {
-        ArrayList _liste = new ArrayList();
-        int[] secili = jtable.getSelectedRows();
+		return liste.get(jtable.getSelectedRow());
+	}
 
-        for (int i = 0; i < secili.length; i++) {
-            _liste.add(liste.get(secili[i]));
-        }
+	public ArrayList<Object> getSecili() {
+		ArrayList<Object> _liste = new ArrayList<Object>();
+		int[] secili = jtable.getSelectedRows();
 
-        return _liste;
-    }
+		for (int i = 0; i < secili.length; i++) {
+			_liste.add(liste.get(secili[i]));
+		}
 
-    public void listeGuncelle(ArrayList yeniListe) {
-        this.liste = yeniListe;
-        prepareTable();
-        jtable.updateUI();
-        this.updateUI();
-    }
+		return _liste;
+	}
 
-    public JTable getJTable() {
-        return jtable;
-    }
+	public void listeGuncelle(ArrayList<Object> yeniListe) {
+		this.liste = yeniListe;
+		prepareTable();
+		jtable.updateUI();
+		this.updateUI();
+	}
+
+	public JTable getJTable() {
+		return jtable;
+	}
 }
