dd7e5739d54384574a8e0c8c1b0745bb873973ed
tekrei <tekrei@7196b984-1229-0410-b02e-49f04d2d2cda>
2008-06-01 10:48:23 +0000

    "Connection Pool" gerçekleştirildi.

--- kumanifest_Toplink/net/kodveus/kumanifest/db/DBConnectionManager.java
+++ kumanifest_Toplink/net/kodveus/kumanifest/db/DBConnectionManager.java
@@ -1,25 +1,343 @@
 package net.kodveus.kumanifest.db;
 
+/*
+ * Copyright (c) 1998 by Gefion software.
+ *
+ * Permission to use, copy, and distribute this software for
+ * NON-COMMERCIAL purposes and without fee is hereby granted
+ * provided that this copyright notice appears in all copies.
+ *
+ * Modified by: Tahir Emre Kalaycı, 01/June/2008
+ */
+
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
 
+import net.kodveus.kumanifest.utility.LogHelper;
+
+/**
+ * This class is a Singleton that provides access to one or many connection
+ * pools defined in a Property file. A client gets access to the single instance
+ * through the static getInstance() method and can then check-out and check-in
+ * connections from a pool. When the client shuts down it should call the
+ * release() method to close all open connections and do other clean up.
+ */
 public class DBConnectionManager {
-
 	private static DBParameters dbp;
 
-	// TODO connection pooling?
+	static private DBConnectionManager instance; // The single instance
 
-	public static Connection getConnection() {
-		try {
-			if (dbp == null)
-				dbp = PersistenceXMLReader.readParameters();
-			Class.forName(dbp.dbDriver);
-			return DriverManager.getConnection(dbp.dbURL, dbp.dbUser,
-					dbp.dbPassword);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
+	static private int clients;
+
+	private DBConnectionPool pool;
+
+	/**
+	 * Returns the single instance, creating one if it's the first time this
+	 * method is called.
+	 * 
+	 * @return DBConnectionManager The single instance.
+	 */
+	static synchronized public DBConnectionManager getInstance() {
+		if (instance == null) {
+			instance = new DBConnectionManager();
+		}
+		clients++;
+		return instance;
+	}
+
+	/**
+	 * A private constructor since this is a Singleton
+	 */
+	private DBConnectionManager() {
+		init();
+	}
+
+	/**
+	 * Returns a connection to the named pool.
+	 * 
+	 * @param name
+	 *            The pool name as defined in the properties file
+	 * @param con
+	 *            The Connection
+	 */
+	public void freeConnection(String name, Connection con) {
+		if (pool != null) {
+			pool.freeConnection(con);
 		}
 	}
 
+	/**
+	 * Returns an open connection. If no one is available, and the max number of
+	 * connections has not been reached, a new connection is created.
+	 * 
+	 * @param name
+	 *            The pool name as defined in the properties file
+	 * @return Connection The connection or null
+	 */
+	public Connection getConnection() {
+		if (pool != null) {
+			return pool.getConnection();
+		}
+		return null;
+	}
+
+	/**
+	 * Returns an open connection. If no one is available, and the max number of
+	 * connections has not been reached, a new connection is created. If the max
+	 * number has been reached, waits until one is available or the specified
+	 * time has elapsed.
+	 * 
+	 * @param name
+	 *            The pool name as defined in the properties file
+	 * @param time
+	 *            The number of milliseconds to wait
+	 * @return Connection The connection or null
+	 */
+	public Connection getConnection(String name, long time) {
+		if (pool != null) {
+			return pool.getConnection(time);
+		}
+		return null;
+	}
+
+	/**
+	 * Closes all open connections and deregisters all drivers.
+	 */
+	public synchronized void release() {
+		// Wait until called by the last client
+		if (--clients != 0) {
+			return;
+		}
+
+		pool.release();
+
+	}
+
+	/**
+	 * Creates instances of DBConnectionPool based on the properties. A
+	 * DBConnectionPool can be defined with the following properties:
+	 * 
+	 * <PRE>
+	 * 
+	 * &lt;poolname&gt;.url The JDBC URL for the database &lt;poolname&gt;.user
+	 * A database user (optional) &lt;poolname&gt;.password A database user
+	 * password (if user specified) &lt;poolname&gt;.maxconn The maximal number
+	 * of connections (optional)
+	 * 
+	 * </PRE>
+	 * 
+	 * @param props
+	 *            The connection pool properties
+	 */
+	private void createPool(DBParameters props) {
+		pool = new DBConnectionPool("ConPool", dbp.dbURL, dbp.dbUser,
+				dbp.dbPassword, dbp.maxConnection);
+		log("Initialized pool " + "ConPool");
+	}
+
+	/**
+	 * Loads properties and initializes the instance with its values.
+	 */
+	private void init() {
+		if (dbp == null)
+			dbp = PersistenceXMLReader.readParameters();
+		loadDriver(dbp);
+		createPool(dbp);
+	}
+
+	/**
+	 * Loads and registers all JDBC drivers. This is done by the
+	 * DBConnectionManager, as opposed to the DBConnectionPool, since many pools
+	 * may share the same driver.
+	 * 
+	 * @param props
+	 *            The connection pool properties
+	 */
+	private void loadDriver(DBParameters dbp) {
+		try {
+			Driver driver = (Driver) Class.forName(dbp.dbDriver).newInstance();
+			DriverManager.registerDriver(driver);
+			log("Registered JDBC driver " + dbp.dbDriver);
+		} catch (Exception e) {
+			log("Can't register JDBC driver: " + dbp.dbDriver + ", Exception: "
+					+ e);
+		}
+	}
+
+	/**
+	 * Writes a message to the log file.
+	 */
+	private void log(String msg) {
+		LogHelper.getLogger().info(new Date() + ": " + msg);
+	}
+
+	/**
+	 * Writes a message with an Exception to the log file.
+	 */
+	private void log(Throwable e, String msg) {
+		LogHelper.getLogger().info(new Date() + ": " + msg);
+		LogHelper.getInstance().exception(e);
+	}
+
+	/**
+	 * This inner class represents a connection pool. It creates new connections
+	 * on demand, up to a max number if specified. It also makes sure a
+	 * connection is still open before it is returned to a client.
+	 */
+	class DBConnectionPool {
+		private int checkedOut;
+
+		private Vector freeConnections = new Vector();
+
+		private int maxConn;
+
+		private String name;
+
+		private String password;
+
+		private String URL;
+
+		private String user;
+
+		/**
+		 * Creates new connection pool.
+		 * 
+		 * @param name
+		 *            The pool name
+		 * @param URL
+		 *            The JDBC URL for the database
+		 * @param user
+		 *            The database user, or null
+		 * @param password
+		 *            The database user password, or null
+		 * @param maxConn
+		 *            The maximal number of connections, or 0 for no limit
+		 */
+		public DBConnectionPool(String name, String URL, String user,
+				String password, int maxConn) {
+			this.name = name;
+			this.URL = URL;
+			this.user = user;
+			this.password = password;
+			this.maxConn = maxConn;
+		}
+
+		/**
+		 * Checks in a connection to the pool. Notify other Threads that may be
+		 * waiting for a connection.
+		 * 
+		 * @param con
+		 *            The connection to check in
+		 */
+		public synchronized void freeConnection(Connection con) {
+			// Put the connection at the end of the Vector
+			freeConnections.addElement(con);
+			checkedOut--;
+			notifyAll();
+		}
+
+		/**
+		 * Checks out a connection from the pool. If no free connection is
+		 * available, a new connection is created unless the max number of
+		 * connections has been reached. If a free connection has been closed by
+		 * the database, it's removed from the pool and this method is called
+		 * again recursively.
+		 */
+		public synchronized Connection getConnection() {
+			Connection con = null;
+			if (freeConnections.size() > 0) {
+				// Pick the first Connection in the Vector
+				// to get round-robin usage
+				con = (Connection) freeConnections.firstElement();
+				freeConnections.removeElementAt(0);
+				try {
+					if (con.isClosed()) {
+						log("Removed bad connection from " + name);
+						// Try again recursively
+						con = getConnection();
+					}
+				} catch (SQLException e) {
+					log("Removed bad connection from " + name);
+					// Try again recursively
+					con = getConnection();
+				}
+			} else if (maxConn == 0 || checkedOut < maxConn) {
+				con = newConnection();
+			}
+			if (con != null) {
+				checkedOut++;
+			}
+			return con;
+		}
+
+		/**
+		 * Checks out a connection from the pool. If no free connection is
+		 * available, a new connection is created unless the max number of
+		 * connections has been reached. If a free connection has been closed by
+		 * the database, it's removed from the pool and this method is called
+		 * again recursively.
+		 * <P>
+		 * If no connection is available and the max number has been reached,
+		 * this method waits the specified time for one to be checked in.
+		 * 
+		 * @param timeout
+		 *            The timeout value in milliseconds
+		 */
+		public synchronized Connection getConnection(long timeout) {
+			long startTime = new Date().getTime();
+			Connection con;
+			while ((con = getConnection()) == null) {
+				try {
+					wait(timeout);
+				} catch (InterruptedException e) {
+				}
+				if ((new Date().getTime() - startTime) >= timeout) {
+					// Timeout has expired
+					return null;
+				}
+			}
+			return con;
+		}
+
+		/**
+		 * Closes all available connections.
+		 */
+		public synchronized void release() {
+			Enumeration allConnections = freeConnections.elements();
+			while (allConnections.hasMoreElements()) {
+				Connection con = (Connection) allConnections.nextElement();
+				try {
+					con.close();
+					log("Closed connection for pool " + name);
+				} catch (SQLException e) {
+					log(e, "Can't close connection for pool " + name);
+				}
+			}
+			freeConnections.removeAllElements();
+		}
+
+		/**
+		 * Creates a new connection, using a userid and password if specified.
+		 */
+		private Connection newConnection() {
+			Connection con = null;
+			try {
+				if (user == null) {
+					con = DriverManager.getConnection(URL);
+				} else {
+					con = DriverManager.getConnection(URL, user, password);
+				}
+				log("Created a new connection in pool " + name);
+			} catch (SQLException e) {
+				log(e, "Can't create a new connection for " + URL);
+				return null;
+			}
+			return con;
+		}
+	}
 }
